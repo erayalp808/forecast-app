862a8ee280bd3ff74867e0334cf40b6137376885
Eray Alp <erayalp.e@gmail.com>
2023-06-27 11:12:48 +0300

    region selector dropdown added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 
+const handleChange = (e) => {
+  let regionSelector = e.target;
+  if (e.target.value != 'null') {
+    regionSelector.style = {color: "black"}
+  } else {
+    regionSelector.style = {color: "grey"}
+  }
+}
+
 function Form() {
   return (
-    <div>
-        
-    </div>
+    <form>
+      <select 
+      style={{color: "grey"}} 
+      id='regionSelector' 
+      name="region" 
+      defaultValue='default' 
+      onChange={handleChange}
+      >
+        <option disabled="disabled" value='default'>Select a region</option>
+        <option value="istanbul">Istanbul</option>
+        <option value="Rome">Rome</option>
+        <option value="New-York">New York</option>
+        <option value="Tokyo">Tokyo</option>
+        <option value="Paris">Paris</option>
+        <option value="Berlin">Berlin</option>
+      </select>
+    </form>
   )
 }
 
