a204821bcf9eeb6990c14e51e8891e2be8c06931
Eray Alp <erayalp.e@gmail.com>
2023-06-27 11:29:40 +0300

    RegionProvider and ForecastCards components added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import ForecastCards from './components/ForecastCards';
 import Form from './components/Form';
+import { RegionProvider } from './components/RegionContext';
 
 function App() {
   return (
-    <div className="App">
+    <RegionProvider>
       <Form />
-      
-    </div>
+      <ForecastCards />
+    </RegionProvider>
   );
 }
 
